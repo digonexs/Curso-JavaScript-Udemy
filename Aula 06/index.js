6cbcb1bf698ce4bf18e4d1e5241932c3b673ff14
Rodrigo Cavalcante de Barros <rodrigocavalcantedebarros@hotmail.com>
2022-08-02 21:27:56 -0300

    "Commit Aula 06"

---
+++ Aula 06/index.js
@@ -0,0 +1,12 @@
+let nome; //Cirando variável sem valor atribuido // 
+let nome1 = 'Rodrigo'; //Criando variável que se refere a nome com "Let" e atribuindo / inicializando um valor a ela //
+
+/* Variável LET pode receber diferentes tipos de dados que podem ser modificados ao longo do código. Como o próprio nome
+diz, ela pode variar seu valor */ 
+
+console.log(nome1 , "nasceu em 2001.");
+console.log("Em 2018," , nome1 , "conheceu Beatriz.");
+console.log(nome1 , "casou-se com Beatriz em 2027.");
+console.log("Beatriz teve dois filhos com" , nome1 , "em 2029.");
+console.log("Os filhos de" , nome1 , "chamam-se Nala e Simba.");
+
