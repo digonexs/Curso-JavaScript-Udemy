2c292c0037868b49482c6a16003d84c621065601
Rodrigo Cavalcante de Barros <rodrigocavalcantedebarros@hotmail.com>
2022-08-03 21:26:26 -0300

    "Aula 07 | Exercícios | e pasta Aula 08"

---
+++ Aula 07/exercicios.js
@@ -0,0 +1,31 @@
+/* EXERCÍCIO
+
+APRESENTE NO CONSOLE A SEGUINTE MENSAGEM:
+
+Rodrigo Barros [variável] tem 21 [variável] anos, pesa 70 [variável] KG
+tem 1.80m [variável] de altura e seu IMC é de  XXXX [variável]
+*/
+
+const nome = 'Rodrigo';
+const sobrenome = 'de Barros';
+let idade = 21;
+let anoNascimento = 2001;
+let peso = 70;
+let altura = 1.80;
+let imc = peso / (altura * altura);
+
+//CONCATENANDO UTILIZANDO ,
+console.log(nome , sobrenome , "tem" , idade , "anos, nasceu em", anoNascimento , ", pesa" , peso , "KG");
+console.log("tem" , altura , "M de altura e seu IMC (Indice de Massa Corporal) é de:" , imc);
+
+console.log()//espaço;
+
+// OUTRA FORMA DE CONCATENAR, UTILIZANDO O +
+console.log(nome + sobrenome + "tem" + idade + "anos, nasceu em" + anoNascimento + ", pesa" + peso + "KG");
+console.log("tem" + altura + "M de altura e seu IMC (Indice de Massa Corporal) é de:" + imc);
+
+console.log()//espaço;
+
+// OUTRA FORMA DE CONCATENAR, UTILIZANDO O ` (TEMPLATE STRINGS)
+console.log(`${nome} ${sobrenome} tem ${idade} anos, nasceu em ${anoNascimento} , pesa ${peso}KG`);
+console.log(`tem ${altura}M de altura e seu IMC (Indice de Massa Corporal) é de: ${imc}`);
---
+++ Aula 07/index.js
@@ -0,0 +1,9 @@
+const nome = 'Rodrigo'  // UMA CONST É UMA VARIÁVEL NO QUAL NÃO SE ALTERA O VALOR DURANTE A EXECUÇÃO DO CÓDIGO. 
+const nome2 = 'Beatriz' // É OBRIGATÓRIO DEFINIR UM VALOR ASSIM QUE ELA FOR DECLARADA.
+const nomeCasal = nome + nome2; //PODEMOS CRIAR UMA CONSTANTE QUE SEU VALOR SEJA REFERENTE A OUTRAS CONSTANTES.
+
+
+console.log(nome);
+console.log("O nome fixo do sistema é" , nome , "e o mesmo não pode ser alterado por manipulação!");
+
+console.log(nomeCasal);
